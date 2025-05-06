a12ac5344d2bbc1a624b8700987a83c6a6d2c279
Tung Pham <tung.42@gmail.com>
2025-05-06 07:58:31 +0700

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -80,6 +80,7 @@ const SortableItem = ({ id, label }) => {
       {...attributes}
       {...listeners}
     >
+      <FontAwesomeIcon icon={faSort} className="me-3" />
       {label}
     </div>
   );
