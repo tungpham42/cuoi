fc22abc60bd579625443b6f6aba7e4573cac0787
Tung Pham <tung.42@gmail.com>
2025-05-22 14:47:42 +0700

    Loading...

--- app/dam-cuoi/[slug]/page.js
+++ app/dam-cuoi/[slug]/page.js
@@ -318,7 +318,7 @@ export default function WeddingPage() {
         className="d-flex align-items-center justify-content-center min-vh-100"
       >
         <Spinner animation="border" variant="danger" />
-        <span className="ms-2 h3">Loading...</span>
+        <span className="ms-2 h3">Đang tải...</span>
       </Container>
     );
   }
